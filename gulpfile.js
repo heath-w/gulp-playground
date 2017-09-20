a3c97ed51c969151d2bd599d40f70f086857699f
Heath Williams <awilli01@amgen.com>
2017-09-20 15:44:35 -0400

    more fun gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
 var concat = require( 'gulp-concat' );
+var clean = require( 'gulp-clean' );
 
 // This is simple example
-gulp.task( 'task-name', function() {
+gulp.task( 'say-hi', function() {
 	console.log( 'Hello there!' );
 });
 
 // This task compiles .scss files in /app/scss to /app/css
-gulp.task( 'sass', function() {
+gulp.task( 'sass', [ 'cleanup' ], function() {
 	console.log( 'gulp sass' );
 	return gulp.src( 'app/scss/*.scss' )
 		.pipe( sass() )
-
-		// .pipe( concat( 'dist/css/all.css' ) );
 		.pipe( gulp.dest( 'app/css' ) );
 });
 
-gulp.task( 'concatenate', function() {
+gulp.task( 'concatenate', [ 'sass' ], function() {
 	console.log( 'gulp sass concatenate' );
 	return gulp.src( 'app/css/*.css' )
 		.pipe( concat( 'all.css' ) )
 		.pipe( gulp.dest( 'dist/css/' ) );
 });
 
-gulp.task( 'delete-old-css', function() {
-	console.log( 'gulp delete older css-related files' );
+gulp.task( 'cleanup', function() {
+	console.log( 'gulp delete older dev/dist-related files' );
+	return gulp.src([ 'app/css/*.css', 'dist/css/*.css' ], {read: false})
+		.pipe( clean() );
 });
 	
+gulp.task( 'test-chain', [ 'concatenate' ] );
